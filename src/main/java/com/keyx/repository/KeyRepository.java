39daab540de5601ced817e90ab2bcc511b1c41bb
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-16 00:58:06 +0530

    Create KeyRepository.java

---
+++ src/main/java/com/keyx/repository/KeyRepository.java
@@ -0,0 +1,7 @@
+package com.keyx.repository;
+
+import com.keyx.model.Key;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeyRepository extends JpaRepository<Key, Long> {
+}
