10837aa38a3dae9c06e08a348a4f9be91e174ab5
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-16 00:53:12 +0530

    Create KeyXApplication.java

---
+++ src/main/java/com/keyx/KeyXApplication.java
@@ -0,0 +1,12 @@
+package com.keyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyXApplication.class, args);
+    }
+}
